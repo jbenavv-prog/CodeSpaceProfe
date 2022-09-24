5d7662488e5c93246968846476a2159e4d3b842a
julian camilo benavides vanegas <correoj94@gmail.com>
2022-09-23 20:58:42 -0500

    numeros primos

---
+++ prime-numbers.js
@@ -0,0 +1,18 @@
+function primos(x){
+    let aux = 0;
+    for (let i= 1;i<x +1;i++) {
+        if (x % i == 0){
+            aux++;
+        }
+    }
+    if(aux == 2){
+        document.write('es primo');
+    }else{
+        document.write('es compuesto');
+        
+    }
+
+        
+    
+}
+primos(183323)
